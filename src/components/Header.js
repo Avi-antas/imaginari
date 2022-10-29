de5c02890e1a3deaf0bb2534dd71645af46d64d3
AVISHEK DAS <avishekdas478@gmail.com>
2022-10-29 23:21:56 +0530

    home  and header styling changed

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,30 +1,42 @@
 import { useSelector, useDispatch } from 'react-redux'
 import React from 'react'
-import { faShoppingBag } from '@fortawesome/free-solid-svg-icons'
+import { faShoppingBag, faSearch } from '@fortawesome/free-solid-svg-icons'
 
 import { Link, NavLink } from 'react-router-dom'
 import './App.scss'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 function Header(props) {
     const updatecart = useSelector((state) => state.counterReducer)
-  return (
-    <div className='header'>
-        <div className='logo'>
-            <h1>imaginari</h1>
-        </div>
-        <div className='navbar'>
-            <div className='nav-left'>
-                <NavLink to="/" className='my-nav'>Home</NavLink>
-                <NavLink to="/about" className='my-nav'>About</NavLink>
+    return (
+        <div className='header nav-sticky'>
+            <div className='logo'>
+                <h1>imaginari</h1>
             </div>
-            <div className='nav-right'>
-                <div className='cart'>
-                    <p style={{color:'#ff0052', fontWeight:500}}><FontAwesomeIcon icon={faShoppingBag} className="site-icon"/>MyCart:{updatecart}</p>
+            <div className='navbar'>
+                <div className='nav-left'>
+                    <div className='nav-wrap'>
+                        <NavLink to="/" className='my-nav'>Home</NavLink>
+                        <NavLink to="/about" className='my-nav'>About</NavLink>
+                    </div>
+
+                    <div className='search'>
+                        <input type="text" placeholder='Search product' />
+                        <button>
+                        <FontAwesomeIcon icon={faSearch} className="sar-icon" />
+                        Search
+                        </button>
+                        
+                    </div>
+                </div>
+                <div className='nav-right'>
+
+                    <div className='cart'>
+                        <p><FontAwesomeIcon icon={faShoppingBag} className="site-icon" />&nbsp;My Cart: {updatecart}</p>
+                    </div>
                 </div>
             </div>
         </div>
-    </div>
-  )
+    )
 }
 
 export default Header
