88de0c43470e78cf6668511191edb7f76a9e5a3b
Avishek Das <avishekdas478@gmail.com>
2022-12-01 16:38:01 +0530

    login function added

--- src/components/Admin.js
+++ src/components/Admin.js
@@ -1,47 +1,53 @@
-import React from 'react'
-import { Card, Container, Table } from 'react-bootstrap'
+import React, { useEffect, useState } from "react";
+import { Card, Container, Table } from "react-bootstrap";
 
 function Admin() {
+  const [product, setProduct] = useState([])
+  const [user, setUser] = useState([]);
+  useEffect(()=>{
+    fetch("http://localhost:4000/users").then((data) => {
+      data.json().then((users) => {
+        setUser(users);
+      });
+    });
+  },[])
+
   return (
     <>
-<Container>
-    <Card>
-    <Table striped bordered hover size="sm">
-      <thead>
-        <tr>
-          <th>#</th>
-          <th>First Name</th>
-          <th>Last Name</th>
-          <th>Username</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr>
-          <td>1</td>
-          <td>Mark</td>
-          <td>Otto</td>
-          <td>@mdo</td>
-        </tr>
-        <tr>
-          <td>2</td>
-          <td>Jacob</td>
-          <td>Thornton</td>
-          <td>@fat</td>
-        </tr>
-        <tr>
-          <td>3</td>
-          <td colSpan={2}>Larry the Bird</td>
-          <td>@twitter</td>
-        </tr>
-      </tbody>
-    </Table>
-    </Card>
-</Container>
-
+      <Container>
+        <Card>
+          <Table striped bordered hover size="sm">
+          <tbody>
+           
+              <tr>
+                <th>#</th>
+                <th>User Name</th>
+                <th>User Email / User Phone</th>
+                <th>User City</th>
+                <th>User Address</th>
+              </tr>
+           
+           
+              {
+                user.map((info)=>{
+                  return(
+                    <tr key={info.id}>
+                    <td>{info.id}</td>
+                    <td>{info.name}</td>
+                    <td>{info.email}</td>
+                    <td>{info.city}</td>
+                    <td>{info.address}</td>
+                  </tr>
+                  )
+                })
+              }
+              
+            </tbody>
+          </Table>
+        </Card>
+      </Container>
     </>
-   
-    
-  )
+  );
 }
 
-export default Admin
+export default Admin;
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,54 +1,94 @@
-import React, { useEffect, useState } from 'react'
-import { Modal, Button } from 'react-bootstrap'
+import React, { useEffect, useState } from "react";
+import { Modal, Button } from "react-bootstrap";
 function Login(props) {
-    const [name, setName] = useState("");
-    const [mail, setMail] = useState("");
-    const [city, setCity] = useState("");
-    const [add, setAdd] = useState("");
+  const [name, setName] = useState("");
+  const [mail, setMail] = useState("");
+  const [city, setCity] = useState("");
+  const [add, setAdd] = useState("");
 
-    const [show, setShow] = useState(false);
+  const [show, setShow] = useState(false);
 
-   
-    
-    const getData = (e) =>{
-      console.log({name,mail,city,add})
-    }
-    
+
+  const getData = () => {
+   let data = { name, mail, city, add };
+   console.log(data)
+   fetch("http://localhost:4000/users",{
+    method: "POST",
+    headers:{
+      "Accept":"applicaton/json",
+      "Content-Type":"applicaton/json"
+    },
+   body: JSON.stringify(data)
+   }).then((results)=>{
+    results.json().then((logged)=>{
+      console.log(logged)
+    })
+   })
+  };
+
+
   return (
-    
-
     <>
-
-    <Modal
-      {...props}
-      size="md"
-      aria-labelledby="contained-modal-title-vcenter"
-      centered
-    >
-      <Modal.Header closeButton>
-        <Modal.Title id="contained-modal-title-vcenter">
-          Modal heading
-        </Modal.Title>
-      </Modal.Header>
-      <Modal.Body>
-        <input type="text" placeholder='Enter your name'  class="form-control mb-3" value={name} onChange={(e)=>{setName(e.target.value)}} />
-        <input type="mail" placeholder='Enter your email'  class="form-control mb-3"  value={mail} onChange={(e)=>{setMail(e.target.value)}}/>
-        <select class="form-control mb-3" value={city} onChange={(e)=>{setCity(e.target.value)}}>
+      <Modal
+        {...props}
+        size="md"
+        aria-labelledby="contained-modal-title-vcenter"
+        centered
+      >
+        <Modal.Header closeButton>
+          <Modal.Title id="contained-modal-title-vcenter">
+            Modal heading
+          </Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <input
+            type="text"
+            placeholder="Enter your name"
+            className="form-control mb-3"
+            value={name}
+            onChange={(e) => {
+              setName(e.target.value);
+            }}
+          />
+          <input
+            type="mail"
+            placeholder="Enter your email"
+            className="form-control mb-3"
+            value={mail}
+            onChange={(e) => {
+              setMail(e.target.value);
+            }}
+          />
+          <select
+            className="form-control mb-3"
+            value={city}
+            onChange={(e) => {
+              setCity(e.target.value);
+            }}
+          >
             <option>Kolkata</option>
             <option>Mumbai</option>
             <option>Chennai</option>
             <option>Bangalore</option>
             <option>Delhi</option>
             <option>Noida</option>
-        </select>
-        <input type="text" placeholder='Enter your address'  class="form-control mb-3" value={add} onChange={(e)=>{setAdd(e.target.value)}}/>
-        <button class="form-control bg-dark text-white" onClick={getData}>Login</button>
-      </Modal.Body>
-      
+          </select>
+          <input
+            type="text"
+            placeholder="Enter your address"
+            className="form-control mb-3"
+            value={add}
+            onChange={(e) => {
+              setAdd(e.target.value);
+            }}
+          />
+          <button className="form-control bg-dark text-white" onClick={getData}>
+            Login
+          </button>
+        </Modal.Body>
       </Modal>
     </>
-    
-  )
+  );
 }
 
-export default Login
+export default Login;
